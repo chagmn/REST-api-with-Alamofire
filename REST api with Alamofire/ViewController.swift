20b334a12d192471e89ec7dbc2db9e386d1d8379
chagmn <sopg4@naver.com>
2020-10-17 12:17:13 +0900

    Initial Commit

---
+++ REST api with Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  REST api with Alamofire
+//
+//  Created by 창민 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
